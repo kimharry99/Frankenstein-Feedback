bd2f87e0d44f02ace465bb5b6afcb436356d3752
DESKTOP-DVB8091\kimha <kimharry9927@gmail.com>
2020-07-25 01:13:44 +0900

    샘플 씬의 유니티 작업 완료 (코딩 제외)

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public int i = 0;
+    public GameObject panel;
+    public void OnClick()
+    {
+        panel.SetActive(true);
+    }
+}
