fe641b001178d69d61231180bdb4b8ffea88ed8d
DESKTOP-DVB8091\kimha <kimharry9927@gmail.com>
2020-07-25 20:00:13 +0900

    UIManager 추가

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,14 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Diagnostics;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+public class UIManager : SingletonBehaviour<UIManager>
 {
-    public int i = 0;
-    public GameObject panel;
-    public void OnClick()
+    [Header("Time UI")]
+    public GameObject TextDay;
+    public GameObject TextTime;
+
+    [Header("Energy UI")]
+    public GameObject SliderEnergy;
+
+    [Header("Sub Panels")]
+    public GameObject PanelSetting;
+    public GameObject PanelHome;
+    public GameObject PanelExploration;
+    public GameObject PanelCrafting;
+    public GameObject PanelStorage;
+
+    public void ButtonSettingClicked()
     {
-        panel.SetActive(true);
+        PanelHome.SetActive(false);
+        PanelSetting.SetActive(true);
     }
+
+    public void ButtonExploreClicked()
+    {
+        PanelExploration.SetActive(true);
+        PanelHome.SetActive(false);
+    }
+
+    public void ButtonCraftClicked()
+    {
+        PanelHome.SetActive(false);
+        PanelCrafting.SetActive(true);
+    }
+
+    public void ButtonStorageClicked()
+    {
+        PanelHome.SetActive(false);
+        PanelStorage.SetActive(true);
+    }
+
 }
