5ca26e08f19ca362faf67a9e65ea34aceb8e5c39
kudave20 <dave1315@korea.ac.kr>
2020-07-26 22:14:13 +0900

    탐사씬 UI

---
+++ Assets/Button4.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button4 : MonoBehaviour
+{
+    public void Click()
+    {
+        Debug.Log("버튼 4를 클릭했습니다.");
+    }
+}
