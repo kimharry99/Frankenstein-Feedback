022b19db07b91891a98bbae9e9fc9e96ba33bd95
DESKTOP-DVB8091\kimha <kimharry9927@gmail.com>
2020-07-28 14:32:52 +0900

    탐사에 관련된 오브젝트를 ExplorationScene으로 분리

---
+++ Assets/Scripts/ExplorationUIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExplorationUIManager : SingletonBehaviour<ExplorationUIManager>
+{
+    #region ExplorationPanel methods
+    public void ButtonOption1Clicked()
+    {
+        Debug.Log("버튼 1을 클릭했습니다.");
+    }
+    public void ButtonOption2Clicked()
+    {
+        Debug.Log("버튼 2를 클릭했습니다.");
+    }
+    public void ButtonOption3Clicked()
+    {
+        Debug.Log("버튼 3을 클릭했습니다.");
+    }
+    public void ButtonOption4Clicked()
+    {
+        Debug.Log("버튼 4를 클릭했습니다.");
+    }
+    #endregion
+}
--- Assets/Scripts/HomeUIManager.cs
+++ Assets/Scripts/HomeUIManager.cs
@@ -46,24 +46,7 @@ public class HomeUIManager : SingletonBehaviour<HomeUIManager>
     }
     #endregion
 
-    #region ExplorationPanel methods
-    public void ButtonOption1Clicked()
-    {
-        Debug.Log("버튼 1을 클릭했습니다.");
-    }
-    public void ButtonOption2Clicked()
-    {
-        Debug.Log("버튼 2를 클릭했습니다.");
-    }
-    public void ButtonOption3Clicked()
-    {
-        Debug.Log("버튼 3을 클릭했습니다.");
-    }
-    public void ButtonOption4Clicked()
-    {
-        Debug.Log("버튼 4를 클릭했습니다.");
-    }
-    #endregion
+
     #region CraftingPanel methods
     public void ButtonItemClicked()
     {
