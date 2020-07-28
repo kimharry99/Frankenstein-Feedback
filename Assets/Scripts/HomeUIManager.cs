64bf3e4c293bbc32099291de26f654c8735d6309
DESKTOP-DVB8091\kimha <kimharry9927@gmail.com>
2020-07-28 14:01:48 +0900

    UIManager의 이름을 HomeUIManager로 변경

--- Assets/Scripts/HomeUIManager.cs
+++ Assets/Scripts/HomeUIManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class UIManager : SingletonBehaviour<UIManager>
+public class HomeUIManager : SingletonBehaviour<HomeUIManager>
 {
     [Header("Time UI")]
     public GameObject TextDay;
