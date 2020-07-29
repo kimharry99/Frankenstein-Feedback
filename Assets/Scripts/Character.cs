9ecb0f2fe531726722a9a4d562da223c4caba6be
DESKTOP-DVB8091\kimha <kimharry9927@gmail.com>
2020-07-29 19:57:58 +0900

    Player 클래스의 이름을 Character로 변경

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : SingletonBehaviour<Character>
+{
+    [Header("Body Parts")]
+    public BodyPart head;
+    public BodyPart body;
+    public BodyPart LeftArm;
+    public BodyPart RightArm;
+    public BodyPart LeftLeg;
+    public BodyPart RightLeg;
+
+}
+
