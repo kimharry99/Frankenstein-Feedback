c2a32d32c4b13fdca2a73a803257dc731c63b756
DESKTOP-DVB8091\kimha <kimharry9927@gmail.com>
2020-07-29 19:46:04 +0900

    Player 클래스 추가

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : SingletonBehaviour<Player>
+{
+    [Header("Body Parts")]
+    public GameObject head;
+    public GameObject body;
+    public GameObject LeftArm;
+    public GameObject RightArm;
+    public GameObject LeftLeg;
+    public GameObject RightLeg;
+
+}
+
