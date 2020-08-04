c34ffe099312672efcecbea47df52fa1d25cc7e2
DESKTOP-DVB8091\kimha <kimharry9927@gmail.com>
2020-08-04 13:44:47 +0900

    Item 클래스와 샘플 아이템 에셋 추가

---
+++ Assets/Scripts/General/Item.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Type
+{
+    Corpse,
+    Consumable,
+    Tool,
+    Ingredient
+}
+public enum Grade
+{
+    A,
+    B,
+    C,
+    D
+}
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{ 
+    public int id;
+    public string name;
+    public string description;
+    public Texture2D itemImage;
+
+    public Type type;
+    public Grade grade;
+    public int energyPotential;
+}
