a7b2f872dfb2b30738798dd6537e2b464539f625
kimharry9927 <kimharry9927@gmail.com>
2020-08-22 16:24:26 +0900

    GetBonusItem 구현

--- Assets/Scripts/BodyDisassembly.cs
+++ Assets/Scripts/BodyDisassembly.cs
@@ -15,7 +15,8 @@ public class BodyDisassembly : MonoBehaviour
                 count++;
                 int slotNumber = HomeUIManager.Inst.indexHoldingChest[i];
                 // TODO : dest를 StorageManager로 수정
-                StorageManager.Inst.DeleteFromChest(slotNumber);
+                BodyPart dissassembled = (BodyPart)StorageManager.Inst.DeleteFromChest(slotNumber);
+                GetBonusItem(dissassembled);
             }
         }
 
@@ -33,4 +34,24 @@ public class BodyDisassembly : MonoBehaviour
 
         GameManager.Inst.OnTurnOver(1);
     }
+
+    private void GetBonusItem(BodyPart item)
+    {
+        BonusItemTable bonusItemTable = new BonusItemTable();
+        int probabilityIndex = GetRandomIndex();
+        Slot[] rewardItems = bonusItemTable.GetTableData(item.race, item.grade, probabilityIndex);
+        StorageManager.Inst.AddItemsToChest(rewardItems[0].slotItem, rewardItems[0].slotItemNumber);
+        StorageManager.Inst.AddItemsToChest(rewardItems[1].slotItem, rewardItems[1].slotItemNumber);
+    }
+
+    // 승윤 TODO : 
+    /// <summary>
+    /// 다음의 확률대로 값을 반환한다. 50% : 0, 30% : 1, 15% : 2, 5% : 3
+    /// </summary>
+    /// <returns></returns>
+    private int GetRandomIndex()
+    {
+        return 0;
+    }
+
 }
