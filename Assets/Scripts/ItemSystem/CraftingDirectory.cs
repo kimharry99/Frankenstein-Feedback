84b2502df8458b5bc063a7ca3a3b60109277eaa0
kimharry9927 <kimharry9927@gmail.com>
2020-08-23 14:59:45 +0900

    Recipie 구조체 선언

--- Assets/Scripts/ItemSystem/CraftingDirectory.cs
+++ Assets/Scripts/ItemSystem/CraftingDirectory.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 public class CraftingDirectory
 {
+    [Serializable]
+    public struct Recipie
+    {
+        public string itemName;
+        public string ingredientItemIds;
+        public int resultItemId;
+    }
     /// <summary>
     /// key값 : 재료 아이템의 Sorted list, value값 : 결과 아이템
     /// </summary>
