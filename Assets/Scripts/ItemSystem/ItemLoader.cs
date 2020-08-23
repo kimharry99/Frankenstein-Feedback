139fa5bfbd1e59bc203d7e4f974967fe9ca6504c
kimharry9927 <kimharry9927@gmail.com>
2020-08-23 14:33:48 +0900

    ItemLoader 구현

---
+++ Assets/Scripts/ItemSystem/ItemLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemLoader : SingletonBehaviour<ItemLoader>
+{
+    private Item[] allItems;
+    private Dictionary<int, Item> dicIdItem = new Dictionary<int, Item>();
+
+    protected override void Awake()
+    {
+        base.Awake(); 
+        allItems = Resources.LoadAll<Item>("Items");
+        for (int i = 0; i < allItems.Length; i++)
+        {
+            dicIdItem.Add(allItems[i].id, allItems[i]);
+            //Debug.Log("add " + allItems[i].itemName);
+        }
+        //Debug.Log("3021 : " + GetItemById(3021).itemName);
+        //Debug.Log("1124 : "+GetItemById(1124).itemName);
+    }
+
+    public Item GetItemById(int id)
+    {
+        Item loadedItem;
+        Item originItem = dicIdItem[id];
+        loadedItem = Resources.Load<Item>("Items/" + originItem.type.ToString() + "/" + originItem.itemName);
+        return loadedItem;
+    }
+}
