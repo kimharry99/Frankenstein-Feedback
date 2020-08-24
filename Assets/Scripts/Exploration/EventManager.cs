a3f9eb228ba0e0ab6f6632e6399c6eb9b8039a96
kimharry9927 <kimharry9927@gmail.com>
2020-08-24 20:41:30 +0900

    EventManager 추가

---
+++ Assets/Scripts/Exploration/EventManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventManager : SingletonBehaviour<EventManager>
+{
+    [SerializeField] // for debugging
+    private List<ExplorationEvent> events;
+
+
+    /// <summary>
+    /// @event를 등장시킨다.
+    /// </summary>
+    /// <param name="event"></param>
+    private void AppearEvent(ExplorationEvent @event)
+    {
+        
+    }
+
+    private void Foo()
+    {
+        Debug.Log("foo");
+    }
+}
