de411fdbba44d5c312225e0df5567c1e7c24420f
kimharry9927 <kimharry9927@gmail.com>
2020-08-24 20:41:06 +0900

    ExplorationEvent, ItemDiscoveryEvent 구현

---
+++ Assets/Scripts/Exploration/ExplorationEvent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public abstract class ExplorationEvent : ScriptableObject
+{
+    public enum EventPhase
+    {
+        RandomEncounter,
+        SearchingItem,
+        None = -1
+    }
+    public enum EventType
+    { 
+        RegionDiscovery,
+        ItemDiscovery,
+        /// <summary>
+        /// 조우
+        /// </summary>
+        Encounter,
+        DurabilityDamage,
+    }
+
+
+    public EventPhase phase;
+    public int id;
+    public string eventName;
+    public string titleText;
+    public string content;
+
+    public abstract void Foo();
+}
---
+++ Assets/Scripts/Exploration/ItemDiscoveryEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemDiscoveryEvent : ExplorationEvent
+{
+    public Item foundedItem;
+
+    public override void Foo()
+    {
+        throw new System.NotImplementedException();
+    }
+}
