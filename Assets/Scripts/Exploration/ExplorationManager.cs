b155b9b9e54fd93365b504f5d7e7fe0ea4905724
kimharry9927 <kimharry9927@gmail.com>
2020-08-25 11:31:09 +0900

    PhaseState추가, _nextEvent 필드제거

--- Assets/Scripts/Exploration/ExplorationManager.cs
+++ Assets/Scripts/Exploration/ExplorationManager.cs
@@ -9,13 +9,23 @@ public class ExplorationManager : SingletonBehaviour<ExplorationManager>
         SearchNextEvent,
         EventIsAppeared,
     }
-    ObjectState objectState;
+
+    private enum PhaseState
+    {
+        None = -1,
+        ItemDiscovery1,
+        ItemDiscovery2,
+        RandomEncounter,
+        FinishingExploration,
+    }
+
     [SerializeField] // for debugging
     private List<ExplorationEvent> _events;
 
-    private ExplorationEvent.EventPhase _eventPhase;
+    private ObjectState objectState;
+    private PhaseState phaseState;
     private ExplorationEvent.Region _curentRegion;
-    private ExplorationEvent _currentEvent;
+    private ExplorationEvent _1currentEvent;
 
     private void Start()
     {
@@ -31,7 +41,7 @@ public class ExplorationManager : SingletonBehaviour<ExplorationManager>
     public void InitializeExploration()
     {
         Debug.Log("탐사 초기화");
-        _eventPhase = ExplorationEvent.EventPhase.SearchingItem;
+        phaseState = PhaseState.ItemDiscovery1;
         _curentRegion = ExplorationEvent.Region.City;
         AppearEvent(_events[1]);
     }
@@ -44,7 +54,7 @@ public class ExplorationManager : SingletonBehaviour<ExplorationManager>
             _time += UnityEngine.Time.deltaTime;
             if (_time > _timeInterval)
             {
-                SelectNextEvent();
+                SelectEvent();
                 _time = 0.0f;
             }
         }
@@ -57,31 +67,34 @@ public class ExplorationManager : SingletonBehaviour<ExplorationManager>
     private void AppearEvent(ExplorationEvent @event)
     {
         objectState = ObjectState.EventIsAppeared;
-        _currentEvent = @event;
-        ExplorationUIManager.Inst.NoticeEvent(_currentEvent);
-        ExplorationUIManager.Inst.AddResultOptionsToButton(_currentEvent);
+        ExplorationUIManager.Inst.NoticeEvent(@event);
+        ExplorationUIManager.Inst.AddResultOptionsToButton(@event);
     }
 
     [Header("Debugging Field")]
     [SerializeField]
     private float _timeInterval = 3.0f;
-    private ExplorationEvent _nextEvent;
+    private ExplorationEvent _currentEvent;
     /// <summary>
     /// 다음으로 등장할 Event를 선택하고 등장시킨다.
     /// </summary>
-    public void SelectNextEvent()
+    public void SelectEvent()
     {
         int temp = (int) (UnityEngine.Time.time * 100.0f);
         Random.InitState(temp);
-        _nextEvent = _events[Random.Range(0, _events.Count)];
-        AppearEvent(_nextEvent);
+        _currentEvent = _events[Random.Range(0, _events.Count)];
+        AppearEvent(_currentEvent);
     }
 
+    /// <summary>
+    /// event선택이 종료된 후 후처리를 담당한다.
+    /// </summary>
+    /// <param name="isReturnHome"></param>
     public void FinishEvent(bool isReturnHome = false)
     {
         Debug.Log("이벤트 종료");
         ExplorationUIManager.Inst.RemoveEventsFromButton();
-        _currentEvent = null;
+        _1currentEvent = null;
         if (!isReturnHome)
         {
             StartCoroutine(ExplorationUIManager.Inst.WaitForEncounter(_timeInterval));
