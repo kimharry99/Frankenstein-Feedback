79edd561597bd4865e7b4e70b23b2c10068c7ba3
kimharry9927 <kimharry9927@gmail.com>
2020-08-25 10:26:39 +0900

    FinishingExplorationEvent 클래스 추가

---
+++ Assets/Scripts/Exploration/FinishExplorationEvent.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FinishExplorationEvent : ExplorationEvent
+{
+    /// <summary>
+    /// 다른 지역을 탐사한다.
+    /// </summary>
+    public override void Option0()
+    {
+        ExploreAnother();
+        FinishEvent();
+    }
+
+    private void ExploreAnother()
+    {
+
+    }
+
+    /// <summary>
+    /// 해당 지역을 한 번 더 탐사한다.
+    /// </summary>
+    public override void Option1()
+    {
+        ExploreAgain();
+        FinishEvent();
+    }
+    
+    private void ExploreAgain()
+    {
+
+    }
+
+    /// <summary>
+    /// 연구소로 복귀한다.
+    /// </summary>
+    public override void Option2()
+    {
+        ReturnHome();
+    }
+
+    private void ReturnHome()
+    {
+        GameManager.Inst.ReturnHome();
+    }
+
+    public override void Option3()
+    {
+        FinishEvent();
+    }
+
+
+}
