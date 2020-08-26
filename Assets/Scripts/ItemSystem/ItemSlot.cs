6213fefe6e355ab4f014d26f992420cb55643a6b
kimharry9927 <kimharry9927@gmail.com>
2020-08-26 23:45:15 +0900

    ItemSlot추가

---
+++ Assets/Scripts/ItemSystem/ItemSlot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// 인벤토리의 InvenItemSlot에 붙어서 사용되는 컴포넌트.
+/// </summary>
+public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private bool mouse_over = false;
+    void Update()
+    {
+        if (mouse_over)
+        {
+            Debug.Log("Mouse Over");
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log(eventData.position);
+        mouse_over = true;
+        Debug.Log("Mouse enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouse_over = false;
+        Debug.Log("Mouse exit");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Right)
+        {
+            // 우클릭시 발생하는 일
+        }
+    }
+}
