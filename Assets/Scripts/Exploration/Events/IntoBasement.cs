8c130f89df4e52b3238833deca78abe807556fb8
kimharry9927 <kimharry9927@gmail.com>
2020-08-30 11:57:50 +0900

    지하실 최초 발견 이벤트 구현

---
+++ Assets/Scripts/Exploration/Events/IntoBasement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IntoBasement : RandomEncounterEvent
+{
+    [SerializeField]
+    private Time _time;
+    protected override bool GetIsEnabled()
+    {
+        if (_time.runtimeTime != 0)
+            return false;
+        if (!Player.Inst.NightVision)
+            return false;
+        return base.GetIsEnabled();
+    }
+}
