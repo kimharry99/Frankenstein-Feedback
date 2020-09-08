e739db1c5df2da6f31b471947713079d23bc5d4c
kimharry99 <40153415+kimharry99@users.noreply.github.com>
2020-09-08 16:20:12 +0900

    Update referring value
    Change indexHoldingChest to GetIndexFromTable

--- Assets/Scripts/BodyDisassembly.cs
+++ Assets/Scripts/BodyDisassembly.cs
@@ -13,7 +13,7 @@ public class BodyDisassembly : MonoBehaviour
     public void DisassembleItem(/* some parameters */)
     {
         int count = 0;
-
+        StorageManager sm = StorageManager.Inst;
         ResetBonusItemNum();
 
         for (int i = 29; i >= 0; i--)
@@ -21,9 +21,10 @@ public class BodyDisassembly : MonoBehaviour
             if (HomeUIManager.Inst.imageCheck[i].activeSelf == true)
             {
                 count++;
-                int slotNumber = HomeUIManager.Inst.indexHoldingChest[i];
+                //int slotNumber = HomeUIManager.Inst.indexHoldingChest[i];
+                int slotNumber = sm.GetIndexFromTable(Type.BodyPart, i);
                 // TODO : dest를 StorageManager로 수정
-                BodyPart dissassembled = (BodyPart)StorageManager.Inst.DeleteFromChest(slotNumber);
+                BodyPart dissassembled = (BodyPart)sm.DeleteFromChest(slotNumber);
                 if(dissassembled == null)
                 {
                     Debug.LogError("dissassembled is null");
